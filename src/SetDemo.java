77a68a0283e13dae1fb502e7da334c417935ca43
SaidBenaissa <d.sbenaissa@gmail.com>
2020-01-09 16:36:28 +0100

    Set Data structure update 1

---
+++ src/SetDemo.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class SetDemo {
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet();
+
+        // Here data is not added as indexing approach.
+        // Here we got hashCodes for elements which being added
+        set.add("said");
+        set.add("said");
+        set.add("hicham");
+        set.add("mohamed");
+        set.add("Jim");
+
+        // 1. Data is unique, no redundancy
+        // 2. Data is unordered in output due to hashing
+        System.out.println("set id "+set);
+
+        Iterator<String> itr = set.iterator();
+
+        while (itr.hasNext()){
+            String str = itr.next();
+            System.out.println(str);
+
+        }
+
+        if (set.contains("said")){
+            System.out.println("Said is in the set"+set);
+        }
+
+        // Remove element Jim
+
+        set.remove("Jim");
+        System.out.println("set after removing Jim "+set);
+    }
+}
